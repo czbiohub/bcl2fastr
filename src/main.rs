267bd7865f64d6e71ae1d103917ef322f476d05c
phoenixAja <phoenix@phoenixlogan.net>
2019-06-04 16:24:13 -0700

    WIP: start of test outline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        assert_eq!(main(), "Hello ");
+    }
+}
