28e6e1a721beef214d0dc20b3a380a09591d030b
gabbyshvartsman <gabbyshvartsman@berkeley.edu>
2019-07-11 16:30:06 -0700

    fixed reading the first few CBCLHeader inputs

--- src/cbcl_decoder.rs
+++ src/cbcl_decoder.rs
@@ -1,4 +1,4 @@
-use byteorder::{BigEndian, LittleEndian, ReadBytesExt};
+use byteorder::{LittleEndian, ReadBytesExt};
 use std::{
     fs::File,
     io::{self, Read},
@@ -8,11 +8,11 @@ use std::{
 #[derive(Debug, PartialEq)]
 pub struct CBCLHeader {
     pub version : u16, // H
-    pub header_size : u16, // I
-    pub bits_per_basecall : u16, // B
-    pub bits_per_qscore : u16, // B
-    pub number_of_bins : u16, //I
-    pub bins : Vec<Vec<u16>>,
+    pub header_size : u32, // I
+    pub bits_per_basecall : u8, // B
+    pub bits_per_qscore : u8, // B
+    pub number_of_bins : u32, //I
+    pub bins : Vec<Vec<u32>>, //I
 
 }
 
@@ -20,14 +20,14 @@ pub struct CBCLHeader {
 impl CBCLHeader {
     fn from_reader(mut rdr: impl Read) -> io::Result<Self> {
         let version = rdr.read_u16::<LittleEndian>()?;
-        let header_size = rdr.read_u16::<LittleEndian>()?;
-        let bits_per_basecall = rdr.read_u16::<LittleEndian>()?;
-        let bits_per_qscore = rdr.read_u16::<LittleEndian>()?;
-        let number_of_bins = rdr.read_u16::<LittleEndian>()?;
+        let header_size = rdr.read_u32::<LittleEndian>()?;
+        let bits_per_basecall = rdr.read_u8()?;
+        let bits_per_qscore = rdr.read_u8()?;
+        let number_of_bins = rdr.read_u32::<LittleEndian>()?;
         let mut bins = Vec::new();
-        for _b in 0..number_of_bins*2 {
-            let from = rdr.read_u16::<LittleEndian>()?;
-            let to = rdr.read_u16::<LittleEndian>()?;
+        for _b in 0..number_of_bins {
+            let from = rdr.read_u32::<LittleEndian>()?;
+            let to = rdr.read_u32::<LittleEndian>()?;
             bins.push(vec![from, to]);
         }
 
