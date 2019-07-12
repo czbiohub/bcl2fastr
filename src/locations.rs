4329381fa571e698ae38827bd80bc56e93d2e98b
gabbyshvartsman <gabbyshvartsman@berkeley.edu>
2019-07-11 17:11:29 -0700

    Decoded locs and created locs test for smaller file, need to figure out workaround to num_clusters for test locs

---
+++ src/locations.rs
@@ -0,0 +1,83 @@
+use byteorder::{LittleEndian, ReadBytesExt};
+use std::{
+    fs::File,
+    io::{self, Read},
+};
+
+#[derive(Debug, PartialEq)]
+pub struct Locs {
+    pub locs : Vec<Vec<f32>>, //f32
+}
+
+
+impl Locs {
+    fn from_reader(mut rdr: impl Read) -> io::Result<Self> {
+        let _ = rdr.read_u64::<LittleEndian>()?;
+        let num_clusters = rdr.read_u32::<LittleEndian>()?;
+        let mut locs = Vec::new();
+        for _c in 0..7 { // 7 is a placeholder for where "num_clusters" should be in every version except for the test version
+            let x = rdr.read_f32::<LittleEndian>()?;
+            let y = rdr.read_f32::<LittleEndian>()?;
+            locs.push(vec![x, y]);
+        }
+
+        Ok(Locs {
+            locs,
+        })
+    }
+}
+
+
+pub fn locs_decoder(locs_path: String) -> Locs {
+    let f = File::open(locs_path).unwrap();
+    let locs = Locs::from_reader(f).unwrap();
+    println!("{:#?}", locs);
+    return locs;
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_locs() {
+        let test_file = "src/test_data/test_locs.locs".to_string();
+        let actual_locs : Locs = locs_decoder(test_file);
+        let expected_locs =
+            Locs {
+                locs : vec![
+                    vec![
+                        0.0,
+                        0.0,
+                    ],
+                    vec![
+                        1.8080986,
+                        0.0,
+                    ],
+                    vec![
+                        3.616197,
+                        0.0,
+                    ],
+                    vec![
+                        5.4242954,
+                        0.0,
+                    ],
+                    vec![
+                        7.232394,
+                        0.0,
+                    ],
+                    vec![
+                        9.040493,
+                        0.0,
+                    ],
+                    vec![
+                        10.848592,
+                        0.0,
+                    ],
+                ],
+            };
+        assert_eq!(actual_locs, expected_locs)
+    }
+}
