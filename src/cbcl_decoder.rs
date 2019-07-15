d2c49acb1df630f7ce2b018522d4aead8803c775
gabbyshvartsman <gabbyshvartsman@berkeley.edu>
2019-07-15 11:16:06 -0700

    Finished decoding cbcl header, created test for the small test file

--- src/cbcl_decoder.rs
+++ src/cbcl_decoder.rs
@@ -13,6 +13,10 @@ pub struct CBCLHeader {
     pub bits_per_qscore : u8, // B
     pub number_of_bins : u32, //I
     pub bins : Vec<Vec<u32>>, //I
+    pub num_tile_records : u32, //I
+    pub tile_offsets : Vec<Vec<u32>>, //I [tile number, num clusters, uncompressed
+                                                // block size, compressed block size]
+    pub non_PF_clusters_excluded : u8, //B, converted from u8 to bool
 
 }
 
@@ -30,6 +34,17 @@ impl CBCLHeader {
             let to = rdr.read_u32::<LittleEndian>()?;
             bins.push(vec![from, to]);
         }
+        let num_tile_records = rdr.read_u32::<LittleEndian>()?;
+        let mut tile_offsets = Vec::new();
+        for _t in 0..num_tile_records {
+            let tile_number = rdr.read_u32::<LittleEndian>()?;
+            let num_clusters = rdr.read_u32::<LittleEndian>()?;
+            let uncomp_block_size = rdr.read_u32::<LittleEndian>()?;
+            let comp_block_size = rdr.read_u32::<LittleEndian>()?;
+            tile_offsets.push(vec![tile_number, num_clusters, uncomp_block_size, comp_block_size]);
+        }
+        let non_PF_clusters_excluded = rdr.read_u8()?;
+
 
         Ok(CBCLHeader {
             version,
@@ -38,6 +53,9 @@ impl CBCLHeader {
             bits_per_qscore,
             number_of_bins,
             bins,
+            num_tile_records,
+            tile_offsets,
+            non_PF_clusters_excluded,
         })
     }
 
@@ -59,15 +77,67 @@ mod tests {
 
     #[test]
     fn test_cbclheader() {
-        let test_file = "src/test_data/L001_1_cbcl_header.cbcl".to_string();
+        let test_file = "src/test_data/test_cbcl_header.cbcl".to_string();
         let actual_cbclheader : CBCLHeader = cbcl_decoder(test_file);
         let expected_cbclheader =
             CBCLHeader {
                 version: 1,
                 header_size: 7537,
-                bits_per_basecall: 0,
-                bits_per_qscore: 514,
-                number_of_bins: 4
+                bits_per_basecall: 2,
+                bits_per_qscore: 2,
+                number_of_bins: 4,
+                bins: vec![
+                    vec![
+                        0,
+                        0,
+                    ],
+                    vec![
+                        1,
+                        11,
+                    ],
+                    vec![
+                        2,
+                        25,
+                    ],
+                    vec![
+                        3,
+                        37,
+                    ],
+                ],
+                num_tile_records: 5,
+                tile_offsets: vec![
+                    vec![
+                        1101,
+                        4091904,
+                        2045952,
+                        1353104,
+                    ],
+                    vec![
+                        1102,
+                        4091904,
+                        2045952,
+                        1354714,
+                    ],
+                    vec![
+                        1103,
+                        4091904,
+                        2045952,
+                        1352351,
+                    ],
+                    vec![
+                        1104,
+                        4091904,
+                        2045952,
+                        1349026,
+                    ],
+                    vec![
+                        1105,
+                        4091904,
+                        2045952,
+                        1349369,
+                    ],
+                ],
+                non_PF_clusters_excluded: 0,
             };
         assert_eq!(actual_cbclheader, expected_cbclheader)
     }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod cbcl_decoder;
 fn main() {
     let filename_info = "src/test_data/RunInfo.xml".to_string();
     let filename_params = "src/test_data/test_runparams.xml".to_string();
-    let cbcl_file = "src/test_data/L001_1_cbcl_header.cbcl".to_string();
+    let cbcl_file = "src/test_data/test_cbcl_header.cbcl".to_string();
     parser::parse_run_info(filename_info);
     parser::parse_run_params(filename_params);
     cbcl_decoder::cbcl_decoder(cbcl_file);
