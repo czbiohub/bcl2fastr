6b39e258f4e5fdf0ff2a16dbb5e00a8a57bca5d0
gabbyshvartsman <gabbyshvartsman@berkeley.edu>
2019-07-15 14:51:09 -0700

    Finished decoding filter file, created test for the small test file

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ extern crate serde_xml_rs;
 
 mod parser;
 mod cbcl_decoder;
+mod filter_decoder;
 
 fn main() {
     let filename_info = "src/test_data/RunInfo.xml".to_string();
     let filename_params = "src/test_data/test_runparams.xml".to_string();
     let cbcl_file = "src/test_data/L001_1_cbcl_header.cbcl".to_string();
+    let filter_file = "src/test_data/test_filter.filter".to_string();
     parser::parse_run_info(filename_info);
     parser::parse_run_params(filename_params);
     cbcl_decoder::cbcl_decoder(cbcl_file);
+    filter_decoder::filter_decoder(filter_file);
 }
