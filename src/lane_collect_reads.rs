2f6accb541e5498879ce584c7173b2b30f768053
gabbyshvartsman <gabbyshvartsman@berkeley.edu>
2019-07-15 19:19:19 -0700

    Added skeleton outline of Lane-organization file

---
+++ src/lane_collect_reads.rs
@@ -0,0 +1,57 @@
+use std::Collections::HashMap;
+
+mod parser;
+mod cbcl_decoder;
+mod filter_decoder;
+mod locations;
+
+pub struct Lane {
+    pub cbcl_files : Vec<&str>, // paths to each cbcl file
+    pub headers : Vec<CBCLHeader>, // use cbcl_fils to read in headers
+    pub tiles : Vec<i32>, // tile numbers (all of the tiles in this lane, can be read from any cbcl file (should be same))
+    pub filters : HashMap<i32, Filter>, // [tile_number, Filter object] read filters using filter_decoder
+    pub base_matrix : Vec<Vec<u8>>, // (0 - 4)
+    pub qscore_matrix : Vec<Vec<u8>>, // (0 - 4)
+    pub reads : Vec<Read>, // vec of Read structs built using the base_matrix and qscore_matrix as inputs
+
+}
+
+impl Lane {
+    fn read_cbcl_paths(lane_path) -> Vec<&str> {
+
+    }
+
+    fn read_cbcl_headers(cbcl_files) -> Vec<CBCLHeader> {
+
+    }
+
+    fn read_tile_nums(lane_path) -> Vec<i32> {
+
+    }
+
+    fn read_filters(lane_path, tiles) -> HashMap<i32, Filter> {
+
+    }
+
+    fn decode_base() -> u8 {
+
+    }
+
+    fn decode_qscore() -> u8 {
+
+    }
+
+    fn decode_cbcls(cbcl_files) -> (Vec<Vec<u8>>, Vec<Vec<u8>>) {
+
+    }
+
+    fn apply_filters(filters, base_matrix, q_score_matrix) -> (Vec<Vec<u8>>, Vec<Vec<u8>>) {
+
+    }
+
+    fn collect_reads(filters, base_matrix, q_score_marix) {
+
+    }
+
+
+}
