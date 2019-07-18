7f11f223237ca977c5af2986916b9177ecace3b3
gabbyshvartsman <gabbyshvartsman@berkeley.edu>
2019-07-18 13:30:56 -0700

    Reading in subpaths from a lane directory

--- src/lane_collect_reads.rs
+++ src/lane_collect_reads.rs
@@ -1,4 +1,9 @@
-use std::Collections::HashMap;
+use std::{
+    Collections::HashMap,
+    fs::File,
+    fs,
+    path::Path,
+};
 
 mod parser;
 mod cbcl_decoder;
@@ -6,9 +11,10 @@ mod filter_decoder;
 mod locations;
 
 pub struct Lane {
-    pub cbcl_files : Vec<&str>, // paths to each cbcl file
+    pub cbcl_paths : Vec<String>, // paths to each cbcl file
     pub headers : Vec<CBCLHeader>, // use cbcl_fils to read in headers
     pub tiles : Vec<i32>, // tile numbers (all of the tiles in this lane, can be read from any cbcl file (should be same))
+    pub filter_paths : Vec<String>, // paths to each filter_file
     pub filters : HashMap<i32, Filter>, // [tile_number, Filter object] read filters using filter_decoder
     pub base_matrix : Vec<Vec<u8>>, // (0 - 4)
     pub qscore_matrix : Vec<Vec<u8>>, // (0 - 4)
@@ -18,14 +24,31 @@ pub struct Lane {
 }
 
 impl Lane {
-    fn read_cbcl_paths(lane_path) -> Vec<&str> {
+    // lane_path directory ex: "<run_id>/Data/Intensities/BaseCalls/L001" for Lane 1
+    // run_id ex: 190414_A00111_0296_AHJCWWDSXX
 
+    // reads in paths from the lane_path dir and sorts into subdirectories (cbcl) or files (filters)
+    fn read_subpaths(lane_path : String) -> (Vec<String>, Vec<String>) {
+        let path = Path::new(lane_path);
+        let cbcl_paths = Vec::new();
+        let filter_paths = Vec::new();
+        for entry in fs::read_dir(path) {
+            let subpath = entry.path();
+            let metadata = fs::metadata(&subpath)?;
+            if metadata.is_file() {
+                filter_paths.push(subpath.into_os_string().into_string().unwrap();)
+            } else {
+                cbcl_paths.push(subpath.into_os_string().into_string().unwrap();)
+            }
+        }
+        return (cbcl_paths, filter_paths);
     }
 
     fn read_cbcl_headers(cbcl_files) -> Vec<CBCLHeader> {
 
     }
 
+    // get from run_info
     fn read_tile_nums(lane_path) -> Vec<i32> {
 
     }
@@ -42,6 +65,7 @@ impl Lane {
 
     }
 
+    // output base_matrix, qscore_matrix
     fn decode_cbcls(cbcl_files) -> (Vec<Vec<u8>>, Vec<Vec<u8>>) {
 
     }
@@ -52,9 +76,21 @@ impl Lane {
 
     }
 
-    fn collect_reads(filters, base_matrix, q_score_marix) {
+    fn collect_reads(filters, base_matrix, q_score_matrix) {
 
     }
 
+}
 
+pub fn lane_collect(lane_path: String) -> Lane {
+    let f = File::open(lane_path).unwrap();
+    let (cbcl_paths, filter_paths) = Lane::read_subpaths(lane_path);
+
+    let lane = Lane {
+        cbcl_paths,
+
+    };
+
+    println!("{:#?}", lane);
+    return lane;
 }
