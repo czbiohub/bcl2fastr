2613fd899df306002414901530e451fc73dedc11
phoenixAja <phoenix@phoenixlogan.net>
2019-07-30 16:07:10 -0700

    main.rs small typo fix

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,15 @@ mod filter_decoder;
 mod locations;
 
 fn main() {
-    let filename_info = Path::new("src/test_data/RunInfo.xml");
-    let filename_params = Path::new("src/test_data/test_runparams.xml");
-    let cbcl_file = Path::new("src/test_data/L001_1_cbcl_header.cbcl");
-    let locs_file = Path::new("src/test_data/test_locs.locs");
-    let filter_file = Path::new("src/test_data/test_filter.filter"sh);
-    parser::parse_run_info(filename_info);
-    parser::parse_run_params(filename_params);
+    // let filename_info = Path::new("src/test_data/RunInfo.xml");
+    // let filename_params = Path::new("src/test_data/test_runparams.xml")
+
+    let cbcl_file = Path::new("src/test_data/test_lane/C1.1/L001_2.cbcl");
+    // let locs_file = Path::new("src/test_data/test_locs.locs");
+    // let filter_file = Path::new("src/test_data/test_filter.filter");
+    // parser::parse_run_info(filename_info);
+    // parser::parse_run_params(filename_params);
     cbcl_decoder::cbcl_decoder(cbcl_file);
-    locations::locs_decoder(locs_file);
-    filter_decoder::filter_decoder(filter_file);
+    // locations::locs_decoder(locs_file);
+    // filter_decoder::filter_decoder(filter_file);
 }
