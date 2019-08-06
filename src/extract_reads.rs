cbac00c9a4d54571adb310b1bf277a8abd210321
Gabby Shvartsman <gabby@ndnd.czbiohub.org>
2019-08-06 14:49:53 -0700

    added args for extract_base_matrix

--- src/extract_reads.rs
+++ src/extract_reads.rs
@@ -6,20 +6,26 @@ use crate::glob;
 
 use crate::parser;
 use crate::filter_decoder;
-use crate::locs_decoder;
-use crate::cbcl_header_decoder;
+use crate::locs_decoder::{
+    locs_decoder,
+    Locs,
+};
+use crate::cbcl_header_decoder::{
+    cbcl_header_decoder,
+    CBCLHeader,
+};
 
 
 static LANE_PARTS : u32 = 2;  // supports 2 parts per lane
 
 
-fn extract_base(cbcl_path : &Path, lane_part : u32, tile_idx : u32, loc : (u32, u32)) {
+fn extract_base(cbcl_path : &Path, tile_idx : u32, loc : (u32, u32)) {
 
 }
 
 
-fn extract_base_matrix(cbcl_path : &Path, lane_part : u32, tile_idces : Vec<u32>, locs : Vec<(u32, u32)>) {
-
+fn extract_base_matrix(header : CBCLHeader, cbcl_path : &Path, tile_idces : Vec<u32>, locs : Locs) {
+    
 }
 
 // dcbcl is decompressed matrix of bases that is passed in to a process
@@ -30,10 +36,10 @@ fn get_read(dcbcl : Vec<Vec<u32>>, indices : (Vec<u32>, Vec<u32>), tile_idx : u3
 
 
 // extracts the reads for a particular lane and is then able to pass those into processes
-pub fn extract_reads(locs_path: &Path, run_info_path: &Path, run_params_path: &Path, lane_path: &Path) {
+pub fn extract_reads(locs_path: &Path, run_info_path: &Path, run_params_path: &Path, lane_path: &Path, tile_idces : Vec<u32>) {
 
     // read in metadata for the run: locs path, run info path, run params paths
-    let locs = locs_decoder::locs_decoder(locs_path);
+    let locs = locs_decoder(locs_path);
     let run_info = parser::parse_run_info(run_info_path);
     let run_params = parser::parse_run_params(run_params_path);
 
@@ -65,7 +71,7 @@ pub fn extract_reads(locs_path: &Path, run_info_path: &Path, run_params_path: &P
         for part_path in &lane_part_paths {
             // &part_path.as_ref() changes the value to be unwrapped from &Option<T> to &Option<&T> which doesn't assume ownership
             // println!("{:#?}", &part_path.as_ref().unwrap());
-            lane_part_headers.push(cbcl_header_decoder::cbcl_header_decoder(&part_path.as_ref().unwrap()));
+            lane_part_headers.push(cbcl_header_decoder(&part_path.as_ref().unwrap()));
         }
 
         cbcl_paths.push(vec!(lane_part_paths));
@@ -88,6 +94,17 @@ pub fn extract_reads(locs_path: &Path, run_info_path: &Path, run_params_path: &P
     }
     println!("{:#?}", filters);
 
+    // 
+    // Note: could just get rid of headers matrix and get each header as you go through that cbcl file
+    let flat_headers = headers.iter().flatten();
+    let flat_paths = cbcl_paths.iter().flatten();
+    for cbcl_data in &flat_headers.zip(&flat_paths) {
+        let header = cbcl_data.0.as_ref().unwrap();
+        let path = cbcl_data.1.as_ref().unwrap();
+  
+        extract_base_matrix(header, path, tile_idces, locs)
+    } 
+    
 
 
 }
