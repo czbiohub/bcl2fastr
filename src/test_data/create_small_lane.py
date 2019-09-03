1e902bdcfc328a95dbfe64b7bbe1dcf1dd9b72c4
phoenixAja <phoenix@phoenixlogan.net>
2019-09-03 16:53:25 -0700

    small sys.argv change for make file

--- src/test_data/create_small_lane.py
+++ src/test_data/create_small_lane.py
@@ -3,6 +3,7 @@ import struct
 import os
 import glob
 import shutil
+import sys
 
 
 def create_small_lane(test_path, lane_path, wanted_num_tiles):
@@ -75,5 +76,7 @@ def create_small_lane(test_path, lane_path, wanted_num_tiles):
 
             
 if __name__ == "__main__":
-    create_small_lane("/usr/src/bcl2fastr/src/test_data", "/usr/src/bcl2fastr_data/L001", 1)
+    save_path = sys.argv[1]
+    data_path = sys.argv[2]
+    create_small_lane(save_path, data_path, 1)
 
