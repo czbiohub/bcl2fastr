e0fb7a9ea1e86783dcd92e7c4237253345140c97
James Webber <james.webber@czbiohub.org>
2019-10-23 15:59:46 -0700

    main() was indented with 3 spaces and that's just wrong

--- src/main.rs
+++ src/main.rs
@@ -14,61 +14,61 @@ mod novaseq_run;
 
 /// Parses command line arguments and runs demux
 fn main() {
-    let matches = App::new("bcl2fastr")
-        .version(clap::crate_version!())
-        .arg(Arg::with_name("run-path")
-             .long("run-path")
-             .help("specify path to the sequencing run folder")
-             .takes_value(true)
-             .required(true))
-        .arg(Arg::with_name("samplesheet")
-             .long("samplesheet")
-             .help("path to samplesheet.csv")
-             .takes_value(true)
-             .required(true))
-        .arg(Arg::with_name("output")
-             .long("output")
-             .help("output path for fastq files")
-             .takes_value(true)
-             .required(true))
-        .arg(Arg::with_name("threads")
-             .long("threads")
-             .help("number of threads used for demultiplexing")
-             .default_value("4")
-             .takes_value(true))
-        .arg(Arg::with_name("tile-chunk")
-             .long("tile-chunk")
-             .help("number of tiles to extract at a time")
-             .default_value("2")
-             .takes_value(true))
-        .arg(Arg::with_name("split-lanes")
-             .long("split-lanes")
-             .help("flag to split output by lane")).
-        get_matches();
+     let matches = App::new("bcl2fastr")
+          .version(clap::crate_version!())
+          .arg(Arg::with_name("run-path")
+               .long("run-path")
+               .help("specify path to the sequencing run folder")
+               .takes_value(true)
+               .required(true))
+          .arg(Arg::with_name("samplesheet")
+               .long("samplesheet")
+               .help("path to samplesheet.csv")
+               .takes_value(true)
+               .required(true))
+          .arg(Arg::with_name("output")
+               .long("output")
+               .help("output path for fastq files")
+               .takes_value(true)
+               .required(true))
+          .arg(Arg::with_name("threads")
+               .long("threads")
+               .help("number of threads used for demultiplexing")
+               .default_value("4")
+               .takes_value(true))
+          .arg(Arg::with_name("tile-chunk")
+               .long("tile-chunk")
+               .help("number of tiles to extract at a time")
+               .default_value("2")
+               .takes_value(true))
+          .arg(Arg::with_name("split-lanes")
+               .long("split-lanes")
+               .help("flag to split output by lane")).
+          get_matches();
 
-    let run_path = PathBuf::from(matches.value_of("run-path").unwrap());
-    if !run_path.exists() {
-         panic!("Could not find run path {}", run_path.display());
-    }
+     let run_path = PathBuf::from(matches.value_of("run-path").unwrap());
+     if !run_path.exists() {
+          panic!("Could not find run path {}", run_path.display());
+     }
 
-    let samplesheet = PathBuf::from(matches.value_of("samplesheet").unwrap());
-    if !samplesheet.exists() {
-         panic!("Could not find samplesheet {}", samplesheet.display());
-    }
+     let samplesheet = PathBuf::from(matches.value_of("samplesheet").unwrap());
+     if !samplesheet.exists() {
+          panic!("Could not find samplesheet {}", samplesheet.display());
+     }
 
-    let output = PathBuf::from(matches.value_of("output").unwrap());
-    if !output.exists() {
-         panic!("Could not find output path {}", output.display());
-    }
+     let output = PathBuf::from(matches.value_of("output").unwrap());
+     if !output.exists() {
+          panic!("Could not find output path {}", output.display());
+     }
 
-    let _threads = value_t!(matches, "threads", u32).unwrap_or_else(|e| e.exit());
-    let tile_chunk = value_t!(matches, "tile-chunk", usize).unwrap_or_else(|e| e.exit());
-    let _split_lanes = matches.is_present("split-lanes");
+     let _threads = value_t!(matches, "threads", u32).unwrap_or_else(|e| e.exit());
+     let tile_chunk = value_t!(matches, "tile-chunk", usize).unwrap_or_else(|e| e.exit());
+     let _split_lanes = matches.is_present("split-lanes");
 
-    let novaseq_run = match novaseq_run::NovaSeqRun::read_path(run_path, tile_chunk) {
-        Ok(n_run) => n_run,
-        Err(e) => panic!("Error reading NovaSeq run: {}", e),
-    };
+     let novaseq_run = match novaseq_run::NovaSeqRun::read_path(run_path, tile_chunk) {
+          Ok(n_run) => n_run,
+          Err(e) => panic!("Error reading NovaSeq run: {}", e),
+     };
 
-    extract_reads::extract_samples(novaseq_run, output).unwrap();
+     extract_reads::extract_samples(novaseq_run, output).unwrap();
 }
